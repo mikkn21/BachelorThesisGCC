875e8847d5eb222d4c7a37951897649eea1fb1e8
Jonas <jonas10bork@gmail.com>
2024-02-10 00:29:04 +0100

    Added clarifying comment

--- tests/testing_tests.cpp
+++ tests/testing_tests.cpp
@@ -2,6 +2,8 @@
 #define BOOST_TEST_MODULE TestingTests
 #include <boost/test/unit_test.hpp>
 
+// Just a simple test to make sure the testing framework works
+
 BOOST_AUTO_TEST_CASE(AlwaysTrue)
 {
     BOOST_CHECK(true);
