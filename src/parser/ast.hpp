9671f53a80c42ad47e0add6bb9369a42d0a0552e
Baneboo <mikkel_10@hotmail.com>
2024-02-13 14:37:04 +0100

    for switching branch Sofus

--- src/parser/ast.hpp
+++ src/parser/ast.hpp
@@ -8,7 +8,34 @@
 namespace grammar 
 { 
     namespace ast
-    {        
+    {     
+        struct PrimitiveType {
+            std::string type;
+        };
+
+        struct Id {
+            std::string id;
+        };
+
+        struct Type {
+          PrimitiveType primitive_type;  
+        };
+
+        struct VarDecl {
+          Type type;
+          Id id;
+          int exp;
+        }; 
+          
+        struct Decl {
+            VarDecl var_decl; 
+        };
+
+        struct Prog {
+            Decl decl; 
+        };
+
+
         struct BinopExp
         { 
             int lhs;
@@ -28,11 +55,26 @@ namespace grammar
 //                          (int, rhs),
 //                           );
 
+using namespace grammar::ast;
+
 BOOST_FUSION_ADAPT_STRUCT(grammar::ast::BinopExp,
-                         lhs,
-                         op,
-                         rhs
+                         (int, lhs),
+                         (std::string, op),
+                         (int, rhs)
                           );
+
+BOOST_FUSION_ADAPT_STRUCT(Prog, (Decl, decl) );
+BOOST_FUSION_ADAPT_STRUCT(Decl, (VarDecl, var_decl) );
+BOOST_FUSION_ADAPT_STRUCT(VarDecl, 
+                         (Type, type),
+                         (Id, id),
+                         (int, exp),
+                         );
+
+BOOST_FUSION_ADAPT_STRUCT(Type, (PrimitiveType, primitive_type) );
+BOOST_FUSION_ADAPT_STRUCT(Id, (std::string, id) );
+BOOST_FUSION_ADAPT_STRUCT(PrimitiveType, (std::string, id) );
+
 
 
 #endif
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,4 +1,5 @@
 #include <boost/spirit/home/x3.hpp>
+#include <sys/resource.h>
 #include "ast.hpp"
 
 using namespace std;
@@ -18,6 +19,12 @@ namespace grammar {
 
         // Rules up here:
         const x3::rule<class binop_exp, ast::BinopExp> binop_exp = "binop_exp"; 
+        const x3::rule<class prog, ast::Prog> prog = "prog";
+        const x3::rule<class decl, ast::Decl> decl = "decl"; 
+        const x3::rule<class var_decl, ast::VarDecl> var_decl = "var_decl"; 
+        const x3::rule<class type, ast::Type> type = "type"; 
+        const x3::rule<class id, ast::Id> id = "id"; 
+        const x3::rule<class primitive_type, ast::PrimitiveType> primitive_type = "primitive_type"; 
 
 
         // Define a parser for operators
@@ -26,6 +33,12 @@ namespace grammar {
             x3::string("+") | x3::string("-") | x3::string("*") | x3::string("/") |
             x3::string("%") | x3::string("<") | x3::string(">") | x3::string("&") | x3::string("|");
 
+        const auto prog_def = decl;
+        const auto decl_def = var_decl;
+        const auto var_decl_def = type >> id >> "=" >> int_ >> ";";
+        const auto type_def = primitive_type;
+        const auto primitive_type_def = x3::string("int") | x3::string("bool");
+        const auto id_def = (x3::char_("a-zA-Z_") >> *x3::char_("a-zA-Z_0-9"));
         const auto binop_exp_def = int_ >> operator_parser >> int_;   
 
         BOOST_SPIRIT_DEFINE(binop_exp); 
