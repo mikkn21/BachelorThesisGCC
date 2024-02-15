72f820ccf2380e241aff26f7feed8e8f349161f9
Baneboo <mikkel_10@hotmail.com>
2024-02-15 11:12:42 +0100

    Added some more rules, but problem with or rules added something in comments

--- src/parser/ast.hpp
+++ src/parser/ast.hpp
@@ -3,12 +3,14 @@
 
 #include <boost/fusion/include/io.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <vector>
 
 
 namespace grammar 
 { 
     namespace ast
-    {     
+    {    
+
         struct PrimitiveType {
             std::string type;
         };
@@ -16,20 +18,64 @@ namespace grammar
         struct Id {
             std::string id;
         };
-
+        
         struct Type {
           PrimitiveType primitive_type;  
         };
 
+        /* struct Type : boost::variant<PrimitiveType, ArrayType> { */
+        /*     using base_type::base_type;  */
+        /* };  */
+        
         struct VarDecl {
           Type type;
           Id id;
           int exp;
-        }; 
-          
+        //Expression exp;
+        };
+
         struct Decl {
             VarDecl var_decl; 
         };
+        
+        /* struct Decl : boost::variant<VarDecl, FuncDecl, ClassDecl> { */
+        /*     using base_type::base_type;  */
+        /**/
+        /* }; */
+
+        struct BlockLine {
+            Decl decl; 
+        };
+
+        struct Block {
+            BlockLine block_line;
+
+        };
+
+        struct Parameter {
+            Type type;
+            Id id; 
+        };
+
+        struct ParameterList {
+            std::vector<Parameter> parameter;
+        };
+
+        struct ArrayType {
+            Type type;
+        };     
+
+
+
+        struct FuncDecl {
+            Type type; 
+            Id id; 
+            ParameterList parameter_list; 
+            Block block;
+
+        };
+          
+
 
         struct Prog {
             Decl decl; 
@@ -65,15 +111,26 @@ BOOST_FUSION_ADAPT_STRUCT(BinopExp,
 
 BOOST_FUSION_ADAPT_STRUCT(Prog, (Decl, decl) );
 BOOST_FUSION_ADAPT_STRUCT(Decl, (VarDecl, var_decl) );
+
+BOOST_FUSION_ADAPT_STRUCT(FuncDecl, (Type, type), (Id, id), (ParameterList, parameter_list), (Block, block) );
 BOOST_FUSION_ADAPT_STRUCT(VarDecl, 
                          (Type, type),
                          (Id, id),
                          (int, exp),
                          );
-
-BOOST_FUSION_ADAPT_STRUCT(Type, (PrimitiveType, primitive_type) );
 BOOST_FUSION_ADAPT_STRUCT(Id, (std::string, id) );
+BOOST_FUSION_ADAPT_STRUCT(Type, (PrimitiveType, primitive_type) );
 BOOST_FUSION_ADAPT_STRUCT(PrimitiveType, (std::string, type) );
+
+BOOST_FUSION_ADAPT_STRUCT(ArrayType, (Type, type), (Id, id) );
+
+BOOST_FUSION_ADAPT_STRUCT(ParameterList, (std::vector<Parameter> paramete) );
+
+BOOST_FUSION_ADAPT_STRUCT(Parameter, (Type, type), (Id, id) );
+
+BOOST_FUSION_ADAPT_STRUCT(Block, (BlockLine, block_line));
+BOOST_FUSION_ADAPT_STRUCT(BlockLine, (Decl, decl));
+
 
 
 
