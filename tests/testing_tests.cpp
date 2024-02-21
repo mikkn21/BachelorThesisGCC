7ce5460f0eddc9e24049445c9e3099b6524b4e9a
sofus <sofus@laubel.net>
2024-02-21 12:10:54 +0100

    temp

--- tests/testing_tests.cpp
+++ tests/testing_tests.cpp
@@ -12,19 +12,22 @@ enum class TestingOutcome {
     SUCCESS,
     PARSE1_FAILED,
     PARSE2_FAILED,
-    PARSE1_PARTIAL_SUCCESS,
-    PARSE2_PARTIAL_SUCCESS,
     PARSE1_INCONSISTENT,
     PARSE2_INCONSISTENT,
 };
 
-void test_parse(std::string input, TestingOutcome testing_outcome) {
-    // grammar::ast::Prog ast1 = parse(input); 
-    // grammar::ast::Prog ast2 = parse(input); 
-    std::string_view h = "hdf";
-    grammar::ast::Prog ast1 = grammar::parser::parse(h);
-    //grammar::ast::Prog ast2 = grammar::parser::parse("");
-
+template <typename T>
+void test_compile_string(std::string input, TestingOutcome testing_outcome, T exception) {
+    try {
+        grammar::ast::Prog ast1 = grammar::parser::parse(input); // should call compile
+    } catch (T exception) {
+        std::cout << exception.what() << endl;
+    }
+    std::ostringstream temp;
+    temp << ast1;
+    std::string ast1_string = temp.str();
+    grammar::ast::Prog ast2 = grammar::parser::parse(ast1_string); // should call compile not parse
+    
 
     // grammar::ast::Prog result1; // TODO remove. Should not be of type BinopExp, should be type Prog
     // auto input_begin = input.begin();
