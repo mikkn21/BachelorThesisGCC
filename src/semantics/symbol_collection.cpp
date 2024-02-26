91814a09561cbc4f7019590879962b4ce364041f
HyperTTPS <jonas10bork@gmail.com>
2024-02-26 13:45:08 +0100

    Working new setup

--- src/semantics/symbol_collection.cpp
+++ src/semantics/symbol_collection.cpp
@@ -5,9 +5,29 @@
 
 using boost::apply_visitor;
 
-class SymbolCollectionVisitor : boost::static_visitor<> {
+class Visitor {
 public: 
-    SymbolCollectionVisitor() {}
+    Visitor() {}
+
+    virtual void ProgPreDecl(const Prog &prog) { }
+    virtual void ProgPostDecl(const Prog &prog) { }
+};
+
+class SymbolCollectionVisitor : public Visitor {
+public: 
+    SymbolCollectionVisitor() : Visitor() {}
+
+    void ProgPostDecl(const Prog &prog) override {
+        std::cout << "symbol collection prog post decl" << std::endl;
+    }
+};
+
+class TreeTraveler : boost::static_visitor<> {
+private: 
+    Visitor &thing;
+
+public:
+    TreeTraveler(Visitor &thing) : thing(thing) {}
 
     template <typename T>
     void operator()(const T &t) {
@@ -16,19 +36,21 @@ public:
 };
 
 template <>
-void SymbolCollectionVisitor::operator()(const Decl &decl) {
-    std::cout << "Visited decl" << std::endl;
+void TreeTraveler::operator()(const Decl &decl) {
+    //this->thing.Decl(decl);
 }
 
 template <>
-void SymbolCollectionVisitor::operator()(const Prog &prog) {
-    std::cout << "Visited prog" << std::endl;
+void TreeTraveler::operator()(const Prog &prog) {
+    this->thing.ProgPreDecl(prog);
     (*this)(prog.decl);
+    this->thing.ProgPostDecl(prog);
 }
 
 
 Prog symbol_collection(Prog &prog) {
-    auto visitor = SymbolCollectionVisitor();
+    auto thing = SymbolCollectionVisitor();
+    auto visitor = TreeTraveler(thing);
     visitor(prog);
     return prog;
 }
