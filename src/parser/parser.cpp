2fef42a78bb31ebd0417e47079d9f555396233d1
sofus <sofus@laubel.net>
2024-02-27 10:16:51 +0100

    stricter grammar

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -13,27 +13,27 @@ namespace grammar {
         // using context_type = x3::phrase_parse_context<x3::ascii::space_type>::type;
         // BOOST_SPIRIT_INSTANTIATE(BinopExp_type, iterator_type, context_type);
 
-        namespace x3 = boost::spirit::x3; 
+        namespace x3 = boost::spirit::x3;
 
         using x3::int_;
         using x3::bool_;
         using namespace grammar::ast;
 
         // Rules up here:
-        const x3::rule<class binop_exp, ast::BinopExp> binop_exp = "binop_exp"; 
-        const x3::rule<class id, ast::Id> id = "id"; 
-        const x3::rule<class primitive_type, ast::PrimitiveType> primitive_type = "primitive_type"; 
-        const x3::rule<class block_line, ast::BlockLine> block_line = "block_line"; 
-        const x3::rule<class block, ast::Block> block = "block"; 
-        const x3::rule<class type, ast::Type> type = "type"; 
-        const x3::rule<class var_decl, ast::VarDecl> var_decl = "var_decl"; 
-        const x3::rule<class parameter, ast::Parameter> parameter = "parameter"; 
-        const x3::rule<class parameter_list, ast::ParameterList> parameter_list = "parameter_list"; 
-        const x3::rule<class func_decl, ast::FuncDecl> func_decl = "func_decl"; 
-        const x3::rule<class array_type, ast::ArrayType> array_type = "array_type"; 
-        const x3::rule<class var_assign, ast::VarAssign> var_assign = "var_assign"; 
-        const x3::rule<class while_statement, ast::WhileStatement> while_statement = "while_statement"; 
-        const x3::rule<class decl, ast::Decl> decl = "decl"; 
+        const x3::rule<class binop_exp, ast::BinopExp> binop_exp = "binop_exp";
+        const x3::rule<class id, ast::Id> id = "id";
+        const x3::rule<class primitive_type, ast::PrimitiveType> primitive_type = "primitive_type";
+        const x3::rule<class block_line, ast::BlockLine> block_line = "block_line";
+        const x3::rule<class block, ast::Block> block = "block";
+        const x3::rule<class type, ast::Type> type = "type";
+        const x3::rule<class var_decl, ast::VarDecl> var_decl = "var_decl";
+        const x3::rule<class parameter, ast::Parameter> parameter = "parameter";
+        const x3::rule<class parameter_list, ast::ParameterList> parameter_list = "parameter_list";
+        const x3::rule<class func_decl, ast::FuncDecl> func_decl = "func_decl";
+        const x3::rule<class array_type, ast::ArrayType> array_type = "array_type";
+        const x3::rule<class var_assign, ast::VarAssign> var_assign = "var_assign";
+        const x3::rule<class while_statement, ast::WhileStatement> while_statement = "while_statement";
+        const x3::rule<class decl, ast::Decl> decl = "decl";
         const x3::rule<class prog, ast::Prog> prog = "prog";
         const x3::rule<class expression,  ast::Expression> expression = "expression";
         const x3::rule<class expression_par, ast::ExpressionPar> expression_par = "expression_par";
@@ -43,26 +43,25 @@ namespace grammar {
         const x3::rule<class statement, ast::Statement> statement = "statement";
 
         // Define a parser for operators
-        const auto operator_parser = 
+        const auto operator_parser =
             x3::string("==") | x3::string("!=") | x3::string("<=") | x3::string(">=") |
             x3::string("+") | x3::string("-") | x3::string("*") | x3::string("/") |
             x3::string("%") | x3::string("<") | x3::string(">") | x3::string("&") | x3::string("|");
-        
+
         // Useable
         const auto primitive_type_def = x3::string("int") | x3::string("bool");
         const auto type_def = primitive_type;  // | array_type;
         const auto id_def = x3::raw[ x3::lexeme[(x3::char_("a-zA-Z_") >> *x3::char_("a-zA-Z_0-9"))]];
         const auto parameter_def = type >> id;
-        const auto parameter_list_def = -(parameter % ','); // -(*(parameter >> ',') >> parameter);
-        const auto expression_par_def = '(' >> expression >> ')';
+        const auto parameter_list_def = -(parameter % ','); //-(*(parameter >> ',') >> parameter);
+        const auto expression_par_def = '(' >> expression > ')';
         const auto expression_base = expression_par | id | int_ | bool_;
-        const auto expression_def = binop_exp | expression_base; 
+        const auto expression_def = binop_exp | expression_base;
         const auto binop_exp_def = expression_base >> (operator_parser > expression);
-
-
-        const auto var_assign_def = id >> '=' >> expression >> ';';
-        const auto while_statement_def = x3::lit("while") >> expression >> block; 
         
+        const auto var_assign_def = id >> '=' >> expression > ";";
+        const auto while_statement_def = x3::lit("while") > expression > block;
+
         const auto statement_expression_def = expression >> ';';
         const auto statement_def = var_assign| while_statement | statement_expression;
 
@@ -71,14 +70,14 @@ namespace grammar {
         const auto block_def = '{' > *block_line > '}';
 
 
-        const auto func_decl_def = type >> id >> ('(' > parameter_list >> ')' >> block);
-        const auto var_decl_def = type >> id >> ('=' > expression >> ';');
+        const auto func_decl_def = type >> id >> ('(' > parameter_list > ')' > block);
+        const auto var_decl_def = type >> id >> ('=' > expression > ';');
         const auto decl_def = var_decl | func_decl;
         const auto prog_def = *decl;
-  
+
         // Not useable
-        const auto array_type_def = type >> x3::lit("[]");  
-        const auto function_call_def = id >> '('>> argument_list >> ')'; 
+        const auto array_type_def = type >> x3::lit("[]");
+        const auto function_call_def = id >> '('>> argument_list > ')';
         const auto argument_list_def = -(expression % ',');
 
         BOOST_SPIRIT_DEFINE(
@@ -110,7 +109,7 @@ namespace grammar {
             namespace x3 = boost::spirit::x3;
             using x3::space;
             using grammar::ast::Prog;
-         
+
             Prog ast;
             using PosIter = boost::spirit::line_pos_iterator<std::string_view::const_iterator>;
             const int SpacesPerTabs = 4;
@@ -134,7 +133,7 @@ namespace grammar {
 
             try {
                 bool r = phrase_parse(iter,  PosIter(src.end()), prog, space, ast);
-                
+
                 if(!r || iter !=  PosIter(src.end())) {
                     throw SyntaxError(makeError() + "\nEnd of x3 error.");
                 }
