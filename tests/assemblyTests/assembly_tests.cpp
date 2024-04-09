26b8be9e47fc889e8ac684e27a0d757a93da147b
sofus <sofus@laubel.net>
2024-04-09 14:53:19 +0200

    Fix assembly test cases and add new test case for precedence

--- tests/assemblyTests/assembly_tests.cpp
+++ tests/assemblyTests/assembly_tests.cpp
@@ -4,5 +4,6 @@
 #include "../lib/testing_assembly_lib.hpp"
 
 
-BOOST_AUTO_TEST_CASE(Print) {test_assembly_file("../tests/assemblyTests/test_print.chad", "1\n-1\n0\n1\n0\n9123923\n2\n-1\n1\n0");}
-BOOST_AUTO_TEST_CASE(Binop) {test_assembly_file("../tests/assemblyTests/testBinop.chad", "13\n7\n30\n3\n1\n1\n0\n0\n1\n0\n1\n1\n1\n0\n1\n0");}
+BOOST_AUTO_TEST_CASE(Print) {test_assembly_file("../tests/assemblyTests/test_print.chad", "1\n0\n1\n0\n9123923\n2\n1\n0");}
+BOOST_AUTO_TEST_CASE(Binop) {test_assembly_file("../tests/assemblyTests/testBinop.chad", "13\n7\n30\n3\n1\n1\n0\n0\n1\n0\n1\n1\n0");}
+// BOOST_AUTO_TEST_CASE(Binop) {test_assembly_file("../tests/assemblyTests/testBinop.chad", "13\n7\n30\n3\n1\n1\n0\n0\n1\n0\n1\n1\n1\n0\n1\n0");}
