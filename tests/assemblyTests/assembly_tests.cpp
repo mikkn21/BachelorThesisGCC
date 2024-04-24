8dc56a69720fd1af4eabde07155e42f26e41b40f
HyperTTPS <jonas10bork@gmail.com>
2024-04-24 13:04:21 +0200

    Array tests

--- tests/assemblyTests/assembly_tests.cpp
+++ tests/assemblyTests/assembly_tests.cpp
@@ -16,6 +16,7 @@ BOOST_AUTO_TEST_CASE(NestedWhile) {test_assembly_file("nestedwhile.chad","0\n1\n
 BOOST_AUTO_TEST_CASE(BreakContinue) {test_assembly_file("breakcont.chad","1\n3");}
 BOOST_AUTO_TEST_CASE(DynamicLinking) {test_assembly_file("dynamic_linking.chad","3628800");}
 BOOST_AUTO_TEST_CASE(StaticLinking) {test_assembly_file("static_linking.chad","2\n2\n0\n9\n1\n2\n3\n4");}
+BOOST_AUTO_TEST_CASE(Arrays) {test_assembly_file("test_arrays.chad","42\n17\n29\n8");}
 
 
 
