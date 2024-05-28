4a90debf663ab5d0d3b1f94d6f9342d148af8873
sofus <sofus@laubel.net>
2024-05-28 20:27:42 +0200

    only missing implementing parameters for smart register allocation

--- src/compiler.hpp
+++ src/compiler.hpp
@@ -38,7 +38,7 @@ public:
     bool print_input = false;
     bool print_code_generation = false;
     bool print_register_allocation = false;
-    bool naive_register_allocation = true;
+    bool naive_register_allocation = false;
 };
 
 std::string get_file_content(std::string_view input);
