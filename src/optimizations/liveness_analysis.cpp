a3d57bb28e45b8e8300f321a81272fa43e4dc15d
sofus <sofus@laubel.net>
2024-05-29 20:41:35 +0200

    smart register fucking works!!!!!!

--- src/optimizations/liveness_analysis.cpp
+++ src/optimizations/liveness_analysis.cpp
@@ -107,11 +107,16 @@ void add_def_use_to_block(Block* current_block, Instruction instruction) {
     switch (instruction.operation) {
         case Op::MOVQ:
             if (holds_any_of<GenericRegister, Register>(instruction.args[0].target)) {
-                if (std::holds_alternative<DIR>(instruction.args[1].access_type)) {
-                    current_block->use.insert(get_register_type(instruction.args[0].target));       
-                }
+                current_block->use.insert(get_register_type(instruction.args[0].target));       
             }
-            if (std::holds_alternative<DIR>(instruction.args[1].access_type)) current_block->def.insert(get_register_type(instruction.args[1].target));
+
+            if (std::holds_alternative<IRL>(instruction.args[1].access_type) || std::holds_alternative<IND>(instruction.args[1].access_type)) {
+                current_block->use.insert(get_register_type(instruction.args[1].target));
+            }
+
+            if (std::holds_alternative<DIR>(instruction.args[1].access_type)) { 
+                current_block->def.insert(get_register_type(instruction.args[1].target));
+            }
             break;
         case Op::POPQ:
             current_block->def.insert(get_register_type(instruction.args[0].target));
@@ -192,6 +197,9 @@ void add_def_use_to_block(Block* current_block, Instruction instruction) {
             current_block->use.insert(Register::RSP);
             //current_block->def.insert(Register::RBP);
             break;
+        case Op::RET:
+            current_block->use.insert(Register::RAX);
+            break;
         default:
             if (!instruction.args.empty() && holds_any_of<GenericRegister, Register>(instruction.args[0].target)) {
                 current_block->use.insert(get_register_type(instruction.args[0].target));
@@ -267,4 +275,3 @@ LivenessAnalysis liveness_analysis(const std::list<Instruction> &code) {
     std::reverse(blocks.begin(), blocks.end());
     return blocks;
 }
-
