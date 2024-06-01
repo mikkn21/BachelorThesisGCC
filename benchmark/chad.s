db0a3f7db7f05719a99d93f2e11d29cb19fce1b1
sofus <sofus@laubel.net>
2024-06-01 19:08:17 +0200

    bencmark on sofus pc

--- benchmark/chad.s
+++ benchmark/chad.s
@@ -42,17 +42,56 @@ L0_fibonacci:
 	pushq %r14	# save R14
 	pushq %r15	# save R15
 	# END OF CALLEE SAVE
-	movq 24(%rbp), %rbx	# move argument from stack
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
 
 L1_if_statement:
-	movq %rbx, %r13
+	movq 24(%rbp), %r11	# Generic Register Translation
+	movq -48(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, 24(%rbp)	# move result back to Generic Register
+	movq %r12, -48(%rbp)	# move result back to Generic Register
+	movq -56(%rbp), %r12	# Generic Register Translation
 	movq $1, %r12
+	movq %r12, -56(%rbp)	# move result back to Generic Register
 	xorq %r10, %r10	# start of less than or equal compare
-	cmpq %r12, %r13
+	movq -56(%rbp), %r11	# Generic Register Translation
+	movq -48(%rbp), %r12	# Generic Register Translation
+	cmpq %r11, %r12
+	movq %r11, -56(%rbp)	# move result back to Generic Register
+	movq %r12, -48(%rbp)	# move result back to Generic Register
 	setle %r10b
-	cmpq $1, %r10
+	movq -64(%rbp), %r12	# Generic Register Translation
+	movq %r10, %r12
+	movq %r12, -64(%rbp)	# move result back to Generic Register
+	movq -64(%rbp), %r11	# Generic Register Translation
+	movq -72(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -64(%rbp)	# move result back to Generic Register
+	movq %r12, -72(%rbp)	# move result back to Generic Register
+	movq -72(%rbp), %r12	# Generic Register Translation
+	cmpq $1, %r12
+	movq %r12, -72(%rbp)	# move result back to Generic Register
 	jne L3_else_statement
-	movq %rbx, %rax
+	movq 24(%rbp), %r11	# Generic Register Translation
+	movq %r11, %rax
+	movq %r11, 24(%rbp)	# move result back to Generic Register
 	movq %rbp, %rsp
 	addq $-40, %rsp
 	popq %r15	# restore R15
@@ -67,8 +106,24 @@ L1_if_statement:
 	jmp L2_endif_statement
 
 L3_else_statement:
-	movq %rbx, %r12
-	subq $1, %r12
+	movq 24(%rbp), %r11	# Generic Register Translation
+	movq -80(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, 24(%rbp)	# move result back to Generic Register
+	movq %r12, -80(%rbp)	# move result back to Generic Register
+	movq -88(%rbp), %r12	# Generic Register Translation
+	movq $1, %r12
+	movq %r12, -88(%rbp)	# move result back to Generic Register
+	movq -88(%rbp), %r11	# Generic Register Translation
+	movq -80(%rbp), %r12	# Generic Register Translation
+	subq %r11, %r12
+	movq %r11, -88(%rbp)	# move result back to Generic Register
+	movq %r12, -80(%rbp)	# move result back to Generic Register
+	movq -80(%rbp), %r11	# Generic Register Translation
+	movq -96(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -80(%rbp)	# move result back to Generic Register
+	movq %r12, -96(%rbp)	# move result back to Generic Register
 	pushq %rax	# save rax
 	pushq %rcx	# save rcx
 	pushq %rdx	# save rdx
@@ -79,13 +134,25 @@ L3_else_statement:
 	pushq %r10	# save r10
 	pushq %r11	# save r11
 	pushq %rsp	# save rsp
-	pushq %r12	# pushing register argument
-	movq %rbp, %rax	# calculating static link for function call
-	movq 16(%rax), %rax
-	pushq %rax	# setting static link
+	movq -96(%rbp), %r11	# Generic Register Translation
+	pushq %r11	# pushing register argument
+	movq %r11, -96(%rbp)	# move result back to Generic Register
+	movq -104(%rbp), %r12	# Generic Register Translation
+	movq %rbp, %r12	# calculating static link for function call
+	movq %r12, -104(%rbp)	# move result back to Generic Register
+	movq -104(%rbp), %r11	# Generic Register Translation
+	movq -104(%rbp), %r12	# Generic Register Translation
+	movq 16(%r11), %r12
+	movq %r11, -104(%rbp)	# move result back to Generic Register
+	movq %r12, -104(%rbp)	# move result back to Generic Register
+	movq -104(%rbp), %r11	# Generic Register Translation
+	pushq %r11	# setting static link
+	movq %r11, -104(%rbp)	# move result back to Generic Register
 	call L0_fibonacci
 	addq $16, %rsp	# remove arguments and static link from stack
+	movq -112(%rbp), %r12	# Generic Register Translation
 	movq %rax, %r12	# save result from function call
+	movq %r12, -112(%rbp)	# move result back to Generic Register
 	popq %rsp	# restore rsp
 	popq %r11	# restore r11
 	popq %r10	# restore r10
@@ -96,7 +163,24 @@ L3_else_statement:
 	popq %rdx	# restore rdx
 	popq %rcx	# restore rcx
 	popq %rax	# restore rax
-	subq $2, %rbx
+	movq 24(%rbp), %r11	# Generic Register Translation
+	movq -120(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, 24(%rbp)	# move result back to Generic Register
+	movq %r12, -120(%rbp)	# move result back to Generic Register
+	movq -128(%rbp), %r12	# Generic Register Translation
+	movq $2, %r12
+	movq %r12, -128(%rbp)	# move result back to Generic Register
+	movq -128(%rbp), %r11	# Generic Register Translation
+	movq -120(%rbp), %r12	# Generic Register Translation
+	subq %r11, %r12
+	movq %r11, -128(%rbp)	# move result back to Generic Register
+	movq %r12, -120(%rbp)	# move result back to Generic Register
+	movq -120(%rbp), %r11	# Generic Register Translation
+	movq -136(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -120(%rbp)	# move result back to Generic Register
+	movq %r12, -136(%rbp)	# move result back to Generic Register
 	pushq %rax	# save rax
 	pushq %rcx	# save rcx
 	pushq %rdx	# save rdx
@@ -107,13 +191,25 @@ L3_else_statement:
 	pushq %r10	# save r10
 	pushq %r11	# save r11
 	pushq %rsp	# save rsp
-	pushq %rbx	# pushing register argument
-	movq %rbp, %rax	# calculating static link for function call
-	movq 16(%rax), %rax
-	pushq %rax	# setting static link
+	movq -136(%rbp), %r11	# Generic Register Translation
+	pushq %r11	# pushing register argument
+	movq %r11, -136(%rbp)	# move result back to Generic Register
+	movq -144(%rbp), %r12	# Generic Register Translation
+	movq %rbp, %r12	# calculating static link for function call
+	movq %r12, -144(%rbp)	# move result back to Generic Register
+	movq -144(%rbp), %r11	# Generic Register Translation
+	movq -144(%rbp), %r12	# Generic Register Translation
+	movq 16(%r11), %r12
+	movq %r11, -144(%rbp)	# move result back to Generic Register
+	movq %r12, -144(%rbp)	# move result back to Generic Register
+	movq -144(%rbp), %r11	# Generic Register Translation
+	pushq %r11	# setting static link
+	movq %r11, -144(%rbp)	# move result back to Generic Register
 	call L0_fibonacci
 	addq $16, %rsp	# remove arguments and static link from stack
-	movq %rax, %rbx	# save result from function call
+	movq -152(%rbp), %r12	# Generic Register Translation
+	movq %rax, %r12	# save result from function call
+	movq %r12, -152(%rbp)	# move result back to Generic Register
 	popq %rsp	# restore rsp
 	popq %r11	# restore r11
 	popq %r10	# restore r10
@@ -124,8 +220,29 @@ L3_else_statement:
 	popq %rdx	# restore rdx
 	popq %rcx	# restore rcx
 	popq %rax	# restore rax
-	movq %r12, %rax
-	addq %rbx, %rax
+	movq -112(%rbp), %r11	# Generic Register Translation
+	movq -160(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -112(%rbp)	# move result back to Generic Register
+	movq %r12, -160(%rbp)	# move result back to Generic Register
+	movq -152(%rbp), %r11	# Generic Register Translation
+	movq -168(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -152(%rbp)	# move result back to Generic Register
+	movq %r12, -168(%rbp)	# move result back to Generic Register
+	movq -168(%rbp), %r11	# Generic Register Translation
+	movq -160(%rbp), %r12	# Generic Register Translation
+	addq %r11, %r12
+	movq %r11, -168(%rbp)	# move result back to Generic Register
+	movq %r12, -160(%rbp)	# move result back to Generic Register
+	movq -160(%rbp), %r11	# Generic Register Translation
+	movq -176(%rbp), %r12	# Generic Register Translation
+	movq %r11, %r12
+	movq %r11, -160(%rbp)	# move result back to Generic Register
+	movq %r12, -176(%rbp)	# move result back to Generic Register
+	movq -176(%rbp), %r11	# Generic Register Translation
+	movq %r11, %rax
+	movq %r11, -176(%rbp)	# move result back to Generic Register
 	movq %rbp, %rsp
 	addq $-40, %rsp
 	popq %r15	# restore R15
@@ -149,6 +266,8 @@ main:
 	pushq %r14	# save R14
 	pushq %r15	# save R15
 	# END OF CALLEE SAVE
+	pushq $0	# Setting temporary variable to 0
+	pushq $0	# Setting temporary variable to 0
 	pushq %rax	# save rax
 	pushq %rcx	# save rcx
 	pushq %rdx	# save rdx
@@ -160,11 +279,22 @@ main:
 	pushq %r11	# save r11
 	pushq %rsp	# save rsp
 	pushq $46	# pushing register argument
-	movq %rbp, %rax	# calculating static link for function call
-	movq 16(%rax), %rax
-	pushq %rax	# setting static link
+	movq -48(%rbp), %r12	# Generic Register Translation
+	movq %rbp, %r12	# calculating static link for function call
+	movq %r12, -48(%rbp)	# move result back to Generic Register
+	movq -48(%rbp), %r11	# Generic Register Translation
+	movq -48(%rbp), %r12	# Generic Register Translation
+	movq 16(%r11), %r12
+	movq %r11, -48(%rbp)	# move result back to Generic Register
+	movq %r12, -48(%rbp)	# move result back to Generic Register
+	movq -48(%rbp), %r11	# Generic Register Translation
+	pushq %r11	# setting static link
+	movq %r11, -48(%rbp)	# move result back to Generic Register
 	call L0_fibonacci
 	addq $16, %rsp	# remove arguments and static link from stack
+	movq -56(%rbp), %r12	# Generic Register Translation
+	movq %rax, %r12	# save result from function call
+	movq %r12, -56(%rbp)	# move result back to Generic Register
 	popq %rsp	# restore rsp
 	popq %r11	# restore r11
 	popq %r10	# restore r10
@@ -175,7 +305,7 @@ main:
 	popq %rdx	# restore rdx
 	popq %rcx	# restore rcx
 	popq %rax	# restore rax
-	xorq %rax, %rax
+	movq $0, %rax
 	movq %rbp, %rsp
 	addq $-40, %rsp
 	popq %r15	# restore R15
@@ -196,11 +326,11 @@ printNum:
 	pushq %r15	# save R15
 	# END OF CALLEE SAVE
 	movq %rdi, %rax	# The number
-	xorq %r9, %r9
+	movq $0, %r9	# Counter for chars to write
 
 .LprintNum_convertLoop:
 	# Convert the number to chars
-	xorq %rdx, %rdx
+	movq $0, %rdx
 	movq $10, %rcx
 	idivq %rcx
 	addq $48, %rdx	# '0' is 48
@@ -235,6 +365,7 @@ printNum:
 
 allocate:
 	pushq %rbp
+	movq %rsp, %rbp
 	pushq %rbx	# save RBX
 	pushq %r12	# save R12
 	pushq %r13	# save R13
