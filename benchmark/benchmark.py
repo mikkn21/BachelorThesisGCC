584d04084670175830b93b81ce53bc39ec0d5954
pctea <sofus@laubel.net>
2024-06-01 19:09:02 +0200

    faster pc benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -15,30 +15,38 @@ map_flag = {
     "-E": "no Peephole",
     "-nE": "Naive + no Peephole",
 }
+
 data_frame = pd.DataFrame(columns=['size', 'name', 'time'])
+
 for file in chad_files:
     for flag in flags:
         pre_command = f"./../build/giga {flag} {file}"
         assemble_command = "as chad.s -o chad.o"
         link_command = "ld chad.o -o chad"
         run_command = "./chad"
+
         os.system(pre_command)
         os.system(assemble_command)
         os.system(link_command)
+
         all_times = []
         for i in range(trials):
-
-            now_time = time.time()
+            start_time = time.time()
             os.system(run_command)
             end_time = time.time()
-            all_times.append(end_time - now_time)
+            all_times.append(end_time - start_time)
             print(f"trial {i + 1} of {trials} for {file} with flag {flag} done...")
+
         size = file.split('_')[-1].split('.')[0]
         name = f"{file.split('_')[0]} {map_flag[flag]}"
-        data_frame = data_frame.append({'size': size, 'name': name, 'time': sum(all_times) / trials}, ignore_index=True)
+        new_row = pd.DataFrame({'size': [size], 'name': [name], 'time': [sum(all_times) / trials]})
+        data_frame = pd.concat([data_frame, new_row], ignore_index=True)
+
         print(f"result of benchmark for {file} with flag {flag}: {sum(all_times) / trials} seconds...")
-    
+
     print(f"done benchmarking {file}...")
+
 data_frame.to_csv('benchmark.csv', index=False)
-#save data frame as dat file
+# save data frame as dat file
 data_frame.to_csv('benchmark.dat', sep='\t', index=False)
+
